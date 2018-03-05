eabc74f91c05fa6188c266b18c995ef8323af7aa
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-03-05 17:57:58 +0100

    Added api with getBalance and sendEther

---
+++ poc-api/server.js
@@ -0,0 +1,96 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8484 });
+
+const dirty = require('dirty');
+var identitiesDB = dirty('identities.db');
+
+var Web3 = require('web3');
+
+var settings = require('./settings.js');
+
+var web3 = new Web3(settings.host);
+
+// Function to broadcast messages to all websocket clients
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+// When a new websocket client connects...
+wss.on('connection', function connection(ws) {
+
+    // ignore errors (dropped connections)
+    ws.on('error', function(error) {
+        // do nothing
+    });
+
+    // receive events from websocket client
+    ws.on('message', function incoming(message) {
+        const event = JSON.parse(message);
+
+        switch (event.eventName) {
+            case 'getBalance':
+                getBalance(ws, event.eventData.address);
+                break;
+            case 'sendEther':
+                sendEther(
+                    ws,
+                    event.eventData.from,
+                    event.eventData.to,
+                    event.eventData.amount
+                );
+                break;
+        }
+
+    });
+
+});
+
+getBalance = function(ws, address) {
+    web3.eth.getBalance(address).then((balance) => {
+        
+        const message = JSON.stringify({
+            eventName: 'balance',
+            eventData: {
+                address: address,
+                balance: balance
+            }
+        });
+    
+        ws.send(message);
+    });
+}
+
+sendEther = async function(ws, from, to, value) {
+    const localIdentity = identitiesDB.get(from);
+
+    const trx = {
+        from: from,
+        to: to,
+        chainId: settings.chainId,
+        value: value,
+        gas: 210000
+    };
+
+    var receipt = await web3.eth.accounts.signTransaction(trx, localIdentity.privateKey)
+    .then((sgnTrx) => {
+        return web3.eth.sendSignedTransaction(sgnTrx.rawTransaction);
+    }).then((result) => {
+        return result;
+    }).catch((error) => {
+        throw new Error(error);
+    });
+
+    const message = JSON.stringify({
+        eventName: 'receipt',
+        eventData: receipt
+    });
+
+    ws.send(message);
+
+    getBalance(ws, from);
+}
